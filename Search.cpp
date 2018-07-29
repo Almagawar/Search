3aa7f37b8e85a7c268f96b98492346d2595e8cc0
Anton Erofeev <erofeev.doc@yandex.ru>
2018-07-29 20:19:44 +0300

    Minor update

--- Search.cpp
+++ Search.cpp
@@ -3,35 +3,45 @@
 #include <cstdlib>
 #include <string>
 #include <Windows.h>
+#include <sstream>
 
 using namespace std;
 
-void getFiles(const string& where, string outpath)
+void getFiles(const string& inpath, string outpath, string exn, string flag)
 {
-    string mask = where + "\\*";
-    WIN32_FIND_DATA FindFileData;
-    HANDLE hf;
-    hf = FindFirstFile(mask.c_str(), &FindFileData);
+	string mask = inpath + "\\*";
+	WIN32_FIND_DATA FindFileData;
+	HANDLE hf;
+	hf = FindFirstFile(mask.c_str(), &FindFileData);
 
 	ofstream output;
 	output.open(outpath + "\\data.txt", ios_base::app);
 
-    if (hf != INVALID_HANDLE_VALUE)
-    {
-        do
-        {
-            if (FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
-            {
-                if (strcmp(FindFileData.cFileName,".") == 0 ||
-                    strcmp(FindFileData.cFileName,"..") == 0) continue;
-                getFiles(where + "\\" + FindFileData.cFileName, outpath);
-            }
-            else
-                output << where << "\\" << FindFileData.cFileName << endl;
-
-        } while (FindNextFile(hf, &FindFileData) != 0);
-        FindClose(hf);
-    }
+	if (hf != INVALID_HANDLE_VALUE)
+	{
+		do
+		{
+			if (FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+			{
+				if (strcmp(FindFileData.cFileName, ".") == 0 ||
+					strcmp(FindFileData.cFileName, "..") == 0) continue;
+				getFiles(inpath + "\\" + FindFileData.cFileName, outpath, exn, flag);
+			}
+			else
+			{
+				string buff = FindFileData.cFileName;
+				if (buff.find(exn.c_str()) != std::string::npos)
+				{
+					output << inpath << "\\" << FindFileData.cFileName << endl;
+					if (flag == "0")
+					{
+						break;
+					}
+				}
+			}
+		} while (FindNextFile(hf, &FindFileData) != 0);
+		FindClose(hf);
+	}
 
 	output.close();
 }
@@ -39,14 +49,49 @@ void getFiles(const string& where, string outpath)
 
 int main()
 {
+	setlocale(LC_ALL, "rus");
+
 	string inpath;
-	cout << "Enter search path: ";
+	cout << "¬ведите путь до директории поиска: ";
 	cin >> inpath;
 
 	string outpath;
-	cout << "Enter path for output data: ";
+	cout << "¬ведите путь до места создани€ файла с данными: ";
 	cin >> outpath;
 
-	getFiles(inpath, outpath);
+	string confpath;
+	cout << "¬ведите путь до файла \"conf.txt\": ";
+	cin >> confpath;
 
+	string line;
+	string exn;
+	string flag;
+	ifstream file(confpath + "\\conf.txt");
+
+	while (getline(file, line))
+	{
+		std::stringstream ss(line);
+		while (getline(ss, line, '|'))
+		{
+			if (line == "1" || line == "0")
+			{
+				flag = line;
+			}
+			else
+			{
+				exn = line;
+				continue;
+			}
+			ofstream output;
+			output.open(outpath + "\\data.txt", ios_base::app);
+			output << exn << "\n\n" << endl;
+			getFiles(inpath, outpath, exn, flag);
+			output << "\n" << endl;
+			output.close();
+		}
+	}
+
+	file.close();
+
+	return 0;
 }
